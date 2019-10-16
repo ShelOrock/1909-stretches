3889e6f91089e35db2b20a11a15f072f543b1901
Spagcat <orockshel@gmail.com>
2019-10-16 18:53:11 -0400

    stretch 10/16

--- questions/08_groupBy/groupBy.js
+++ questions/08_groupBy/groupBy.js
@@ -1,3 +1,24 @@
-function groupBy(arr, attr) {}
+function groupBy(arr, attr) {
+    if(typeof attr === 'string') {
+        return arr.reduce((accum, item) => {
+            if(item.length in accum) {
+                accum[item.length].push(item);
+            } else {
+                accum[item.length] = [item];
+            }
+            return accum;
+        }, {});
+    } else {
+        return arr.reduce((accum, item) => {
+            if(Math.floor(item) in accum) {
+                accum[Math.floor(item)].push(item);
+            } else {
+                accum[Math.floor(item)] = [item]
+            }
+            return accum;
+        }, {});
+    }
+
+}
 
 module.exports = { groupBy };
