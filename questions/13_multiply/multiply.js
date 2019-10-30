381ef9c400da732969fc2e7a47cfcc3d972cfcde
Spagcat <orockshel@gmail.com>
2019-10-30 19:04:32 -0400

    10-30 stretch

--- questions/13_multiply/multiply.js
+++ questions/13_multiply/multiply.js
@@ -1,25 +1,6 @@
 const multiply = (num1, num2) => {
-
-    describe('multiply', () => {
-        test('it should receive two numbers', () => {
-
-        expect(() => multiply('poop')).toThrow();
-
-        expect(() => multiply(1, 2, 3)).toThrow();
-        });
-
-        test('it returns the product of two numbers', () => {
-            expect(1 * 1).toBe('a number');
-            expect(10 * 69).toBe(690);
-            expect(20 * 2).toBe(40);
-        });
-
-        test('it is not hardcoded', () => {
-            const a = Math.ceil(Math.random() * 100);
-            const b = Math.ceil(Math.random() * 100);
-            expect(a * b).toBe(a * b);
-        });
-    });
+    if(typeof num1 !== 'number' || typeof num2 !== 'number') throw new Error('bad');
+    return num1 * num2;
 }
 
 
--- questions/13_multiply/multiply.test.js
+++ questions/13_multiply/multiply.test.js
@@ -4,23 +4,25 @@ describe('multiply testing', () => {
   test('it should ensure it receives two numbers', () => {
     // test some values that should fail
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(() => multiply('poop', 420)).toThrow();
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(() => multiply(['i', 'smell', 'like', 'beef'], 666)).toThrow();
   });
 
   test('it returns the product of two numbers', () => {
     // test the type of the returned value
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(1 * 1).toBe(1);
 
     // test that the returned value is correct
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(3 * 5).toBe(15);
 
     // test some other values
-    expect(/* fill this in */).toBe(/* fill this in */);
+    expect(69 * 10).toBe(690);
   });
 
   test('it is not hardcoded (hint: use random numbers)', () => {
-    // test some random values
+    const num1 = Math.ceil(Math.random() * 100);
+    const num2 = Math.ceil(Math.random() * 100);
+    expect(num1 * num2).toBe(num1 * num2)
   });
 });
