7b125768da146092b5d36883f3d4fec339a159da
Spagcat <orockshel@gmail.com>
2019-11-13 19:19:36 -0500

    11-13 stretch

--- questions/15_cache/cache.js
+++ questions/15_cache/cache.js
@@ -1,6 +1,14 @@
 function cache(func) {
     if(typeof func !== 'function') throw new Error('Input must be a function.')
-    
+    let memo = {};
+    return function(...arg) {
+        if(memo[arg] !== undefined) {
+            return memo[arg];
+        } else {
+            memo[arg] = func(...arg)
+            return memo[arg]
+        }
+    }
 }
 
 module.exports = { cache };
